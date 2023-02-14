de2862e89a1f8931d1260833b0c6f9cc71f2e057
Oliwer <oliwerl@outlook.com>
2023-02-14 15:12:08 +0100

    Alternatives added
    Random alternatives

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,10 @@
+random_pet_name = array_random_element(ALL_BREEDS).name;
+random_pet_picture = array_random_element(ALL_BREEDS).url;
+
+function array_random_element(array) {
+  const random_index = Math.floor(array.length * Math.random());
+  return array[random_index];
+}
 function get_quiz() {
   console.log("Quiz started");
   document.querySelector("main").innerHTML = `
@@ -6,5 +13,25 @@ function get_quiz() {
   <p>${input_username.value}</p>
   <button>Logout</button>
   </div>
-  `;
+  <img src="media/logo.png" />
+  <div id="alternatives">
+  <button class="alt">${
+    ALL_BREEDS[random_number(ALL_BREEDS.length)].name
+  }</button>
+  <button class="alt">${
+    ALL_BREEDS[random_number(ALL_BREEDS.length)].name
+  }</button>
+  <button class="alt">${
+    ALL_BREEDS[random_number(ALL_BREEDS.length)].name
+  }</button>
+  <button class="alt">${
+    ALL_BREEDS[random_number(ALL_BREEDS.length)].name
+  }</button>
+</div>
+<div id="image"></div>
+`;
+}
+
+function random_number(max) {
+  return Math.floor(max * Math.random());
 }
